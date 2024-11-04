b85b7204364dc1e69a8eaa933aa2ab65b49e5c38
gyzlic <nikitabrdn@gmail.com>
2024-11-04 15:56:51 +0300

    Update main1.py

--- 3 лаба/main1.py
+++ 3 лаба/main1.py
@@ -1,11 +1,14 @@
-# TODO Напишите функцию для поиска индекса товара
-
+def find_item_index(items, item):
+    try:
+        return items.index(item) 
+    except ValueError:
+        return None  
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
-    try:
-        index_item = items_list.index(find_item)
+    index_item = find_item_index(items_list, find_item)
+    if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
-    except ValueError:
+    else:
         print(f"Товар '{find_item}' не найден в списке.")
