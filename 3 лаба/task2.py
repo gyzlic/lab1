7625d2a835a64405eb46df337753a607268e7b87
gyzlic <nikitabrdn@gmail.com>
2024-11-04 15:51:22 +0300

    Update task2.py

--- 3 лаба/task2.py
+++ 3 лаба/task2.py
@@ -1,8 +1,8 @@
 def find_common_participants(group1, group2, separator=","):
     set_group1 = set(group1.split(separator))
     set_group2 = set(group2.split(separator))
-    common_participants = sorted(set_group1.intersection(set_group2))
-    return common_participants
+    common_participants = set_group1.intersection(set_group2)
+    return sorted(common_participants)
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
